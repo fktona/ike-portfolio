6ccfee1242321812a07f237fdf0126d832e91a0d
fktona <adetona.fk@gmail.com>
2025-05-28 00:27:43 +0100

    jio

--- components/Certificate.tsx
+++ components/Certificate.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 
 const Certificate: FC = () => (
-  <section className="my-8">
+  <section className="my-8 w-full flex flex-col items-center justify-center">
     <h2 className="text-2xl font-bold mb-4">Certificate Course(s)</h2>
-    <div className="bg-gray-100 p-4 rounded shadow">
+    <div className=" p-4 ">
       <h3 className="font-semibold">University of Illinois, Urbana-Champaign</h3>
       <p className="text-sm text-gray-600">February 2024</p>
       <p>Certificate of completion in Introduction to Sustainability</p>
